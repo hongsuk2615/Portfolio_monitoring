1ec584c1abd22d01dd4c850c9014f8a965110f03
김홍석 <hongsuk2615@gmail.com>
2026-02-09 17:14:02 +0900

    fix:배포 테스트 단계에서 Dataloader는 스킵

--- src/main/java/com/portfolio/monitoring/configuration/DataLoader.java
+++ src/main/java/com/portfolio/monitoring/configuration/DataLoader.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("!test")
 public class DataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
